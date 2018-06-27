0b31475b5b68f3850de0aa4562e9070c14310d2f
Gleisson de Assis <gleisson.assis@gmail.com>
2018-06-27 19:55:58 -0300

    Just testing...

--- oz-workspace/truffle.js
+++ oz-workspace/truffle.js
@@ -1,3 +1,13 @@
+require('dotenv').config();
+const Web3 = require("web3");
+const web3 = new Web3();
+const WalletProvider = require("truffle-wallet-provider");
+const Wallet = require('ethereumjs-wallet');
+
+var ropstenPrivateKey = new Buffer("2355aea4ab7bfbd1f3b6118e9c8c287830ea9137beaabe31c1b739c80827353f", "hex")
+var ropstenWallet = Wallet.fromPrivateKey(ropstenPrivateKey);
+var ropstenProvider = new WalletProvider(ropstenWallet, "https://ropsten.infura.io/");
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // for more about customizing your Truffle configuration!
@@ -6,6 +16,16 @@ module.exports = {
       host: "127.0.0.1",
       port: 7545,
       network_id: "*" // Match any network id
-    }
+    },
+    ropsten: {
+      provider: ropstenProvider,
+      // You can get the current gasLimit by running
+      // truffle deploy --network rinkeby
+      // truffle(rinkeby)> web3.eth.getBlock("pending", (error, result) =>
+      //   console.log(result.gasLimit))
+      //gas: 4600000000,
+      gasPrice: web3.toWei("20", "gwei"),
+      network_id: "3",
+    },
   }
 };
